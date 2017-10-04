6e7f93fa59466f1b6d2b959ae1e2fd0885f30075
jjsafs <felipediasc@yahoo.com.br>
2017-10-04 17:46:38 -0300

    Parallax e Login

--- app/src/main/java/correia/felipe/exo_app/Activity_Login.java
+++ app/src/main/java/correia/felipe/exo_app/Activity_Login.java
@@ -3,6 +3,7 @@ package correia.felipe.exo_app;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +27,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Felipe on 04/08/2017.
@@ -34,6 +38,8 @@ import java.io.IOException;
 public class Activity_Login extends AppCompatActivity {
 
     protected static final int TIMER_RUNTIME = 10000;
+    public static final int READ_TIMEOUT=15000;
+    public static final int CONNECTION_TIMEOUT=10000;
     protected boolean mLoading;
     protected ProgressBar mProgressBar;
 
@@ -48,6 +54,7 @@ public class Activity_Login extends AppCompatActivity {
 
     private String FEED_URL = "http://blessp.azurewebsites.net/api/login";
     //private String FEED_URL = "http://192.168.0.14:8000/api/login";
+
 
 
     JSONArray user = null;
@@ -72,13 +79,28 @@ public class Activity_Login extends AppCompatActivity {
 
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                mProgressBar.setVisibility(View.VISIBLE);
-                if (editEmail.getText().length() == 0 || editPassword.getText().length() == 0) {
-                    Toast.makeText(getApplicationContext(), "Digite o usuário e/ou senha",
-                            Toast.LENGTH_LONG).show();
-                } else {
+                //mProgressBar.setVisibility(View.VISIBLE);
+                if (editEmail.getText().length() == 0) {
+                    editEmail.setError("Digite um email");
+                    editEmail.requestFocus();
+                    mProgressBar.setVisibility(View.GONE);
+                    //Toast.makeText(getApplicationContext(), "Digite o usuário e/ou senha", Toast.LENGTH_LONG).show();
+                }
+                if(editPassword.getText().length() == 0) {
+                    editPassword.setError("Digite um email");
+                    editPassword.requestFocus();
+                    mProgressBar.setVisibility(View.GONE);
+                }
+
+                if(editEmail.getText().length() != 0 && editPassword.getText().length() != 0){
+
+                    checkLogin();
+
+                 /*
+
                     final String json = makeJson(editEmail, editPassword);
                     Log.d("Script", "JSON LOGIN: " + json);
                     new Thread() {
@@ -101,28 +123,7 @@ public class Activity_Login extends AppCompatActivity {
                                 }
 
                                 //com API
-                                if (answer == null) {
-                                    Toast.makeText(getApplicationContext(), "Tente novamente", Toast.LENGTH_LONG).show();
-                                } else {
-                                    JSONObject token_json = null;
-                                    try {
-                                        token_json = new JSONObject(answer);
-                                        String token = token_json.getString("token");
-                                        Log.d("LOGIN", "TOKEN: " + token);
-                                        final Token_Item token_item = new Token_Item();
-                                        token_item.setToken(token);
 
-                                        Intent login = new Intent(Activity_Login.this, Activity_Principal.class);
-                                        login.putExtra("token", token_item.getToken());
-                                        startActivity(login);
-
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
-
-                                }
                             } catch (InterruptedException e) {
 
                             } finally {
@@ -131,10 +132,7 @@ public class Activity_Login extends AppCompatActivity {
                             }
                         }
                     }.start();
-/*
-                    //com API
                     if (answer == null) {
-                        mProgressBar.setVisibility(View.GONE);
                         Toast.makeText(getApplicationContext(), "Tente novamente", Toast.LENGTH_LONG).show();
                     } else {
                         JSONObject token_json = null;
@@ -144,7 +142,6 @@ public class Activity_Login extends AppCompatActivity {
                             Log.d("LOGIN", "TOKEN: " + token);
                             final Token_Item token_item = new Token_Item();
                             token_item.setToken(token);
-                            mProgressBar.setVisibility(View.GONE);
 
                             Intent login = new Intent(Activity_Login.this, Activity_Principal.class);
                             login.putExtra("token", token_item.getToken());
@@ -155,8 +152,8 @@ public class Activity_Login extends AppCompatActivity {
                             e.printStackTrace();
                         }
 
-
                     }*/
+
                 }
             }
         });
@@ -256,8 +253,98 @@ public class Activity_Login extends AppCompatActivity {
     }
 
 
+    public void checkLogin(){
 
 
+        if (editEmail.getText().length() == 0) {
+            editEmail.setError("Digite um email");
+            editEmail.requestFocus();
+            mProgressBar.setVisibility(View.GONE);
+            //Toast.makeText(getApplicationContext(), "Digite o usuário e/ou senha", Toast.LENGTH_LONG).show();
+        }
+        if(editPassword.getText().length() == 0) {
+            editPassword.setError("Digite um email");
+            editPassword.requestFocus();
+            mProgressBar.setVisibility(View.GONE);
+        }
+        final String email = editEmail.getText().toString();
+        final String password = editPassword.getText().toString();
+
+        new AsyncLogin().execute(email,password);
+    }
+
+    private class AsyncLogin extends AsyncTask<String, String, Integer>{
+        ProgressDialog pdLoading = new ProgressDialog(Activity_Login.this);
+
+        final String makeJson = makeJson(editEmail, editPassword);
+
+
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+        }
+
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer statusCode = 0;
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(FEED_URL);
+            String answer = null;
+            try {
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setHeader("Content-type", "application/json");
+                StringEntity postString = new StringEntity(makeJson, "UTF-8");
+                httpPost.setEntity(postString);
+
+                HttpResponse resposta = httpClient.execute(httpPost);
+
+                statusCode = resposta.getStatusLine().getStatusCode();
+                Log.d("LOGIN", "Status Code: " + statusCode);
+
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return statusCode;
+        }
+        @Override
+        protected void onPostExecute(Integer result ){
+            pdLoading.dismiss();
+            if (result == 200) {
+                new Thread() {
+                    public void run() {
+                        try {
+                            answer = getSetDataWeb(FEED_URL, makeJson);
+                            JSONObject token_json = new JSONObject(answer);
+                            String token = token_json.getString("token");
+                            Log.d("LOGIN", "TOKEN: " + token);
+                            final Token_Item token_item = new Token_Item();
+                            token_item.setToken(token);
+
+                            Intent login = new Intent(Activity_Login.this, Activity_Principal.class);
+                            login.putExtra("token", token_item.getToken());
+                            startActivity(login);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }else{
+                Toast.makeText(Activity_Login.this, "Status Code: "+result, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
 
 
